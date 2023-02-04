171d18624791db91bfd705664ee51c3628cfe82f
myroslav <myrkozar@gmail.com>
2023-02-04 21:20:02 +0200

    add static images

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,23 @@ import { ProductModule } from './product/product.module'
 
 import { MongooseModule } from '@nestjs/mongoose'
 import { ReviewModule } from './review/review.module'
+import { path } from 'app-root-path'
+import { ServeStaticModule } from '@nestjs/serve-static'
 
+//!ServeStaticModule делаем статичную папку с картинками чтобы она открывалась через url
 const DB_HOST =
   'mongodb+srv://Myroslav:kynYR4hNZ8C60ouB@cluster0.3g8xmkk.mongodb.net/shop?retryWrites=true&w=majority'
 //это корневой файл где собираются все наши модули
 @Module({
-  imports: [ProductModule, ReviewModule, MongooseModule.forRoot(DB_HOST)],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: `${path}/uploads`,
+      serveRoot: `/uploads`
+    }),
+    MongooseModule.forRoot(DB_HOST),
+    ProductModule,
+    ReviewModule
+  ],
   controllers: [],
   providers: []
 })
