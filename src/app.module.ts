f81597f4f42032435e90ee38f5bcd9035a6e2137
myroslav <myrkozar@gmail.com>
2023-02-16 23:12:31 +0200

    add env conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,20 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { ReviewModule } from './review/review.module'
 import { path } from 'app-root-path'
 import { ServeStaticModule } from '@nestjs/serve-static'
+import { ConfigModule } from '@nestjs/config'
 
 //!ServeStaticModule делаем статичную папку с картинками чтобы она открывалась через url
-const DB_HOST =
-  'mongodb+srv://Myroslav:kynYR4hNZ8C60ouB@cluster0.3g8xmkk.mongodb.net/shop?retryWrites=true&w=majority'
+// const DB_HOST =
+//   'mongodb+srv://Myroslav:kynYR4hNZ8C60ouB@cluster0.3g8xmkk.mongodb.net/shop?retryWrites=true&w=majority'
 //это корневой файл где собираются все наши модули
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: `${path}/uploads`,
       serveRoot: `/uploads`
     }),
-    MongooseModule.forRoot(DB_HOST),
+    MongooseModule.forRoot(process.env.DB_HOST_CAFE),
     ProductModule,
     ReviewModule
   ],
